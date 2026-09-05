13c6ba09ed60e1499562e366b0f62fb2cfdb31da
NetoGambogi <netogambogi@hotmail.com>
2026-09-04 22:16:59 -0300

    mudança de framework backend, implementação de login com google em testes web

---
+++ checkfrota-api/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\GoogleAuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/auth/google', [GoogleAuthController::class, 'redirect']);
+Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback']);
